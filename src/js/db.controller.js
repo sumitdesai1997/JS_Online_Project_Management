274393a786a392349c483139fd8f4b1685df584d
Ranjana <ranjana.madt@gmail.com>
2022-02-11 00:41:42 +0530

    Add & get Users list storage methods , login form completed

--- src/js/db.controller.js
+++ src/js/db.controller.js
@@ -1,97 +1,26 @@
-let db = null;
-let objectStore = null;
-let DBOpenReq = indexedDB.open('projectDB', 1);
+let MANAGERS="managers"
+let MEMBERS="members"
 
-DBOpenReq.addEventListener('error', (err) => {
-    //Error occurred while trying to open DB
-    console.warn(err);
-  });
+var getManagers = () => {
+  return JSON.parse(localStorage.getItem(MANAGERS) || "[]");
+}
+var getTeamMembers = () => {
+  return JSON.parse(localStorage.getItem(MEMBERS) || "[]");
+}
 
-  DBOpenReq.addEventListener('success', (ev) => {
-    //DB has been opened... after upgradeneeded
-    db = ev.target.result;
-    console.log('success opening DB');
-    fetchData();
-    insertProducts();
-  });
+function addManagers(managers) {
+  if(getManagers().length==0)
+    localStorage.setItem(MANAGERS, JSON.stringify(managers));
+};
 
-  DBOpenReq.addEventListener('upgradeneeded', (ev) => {
-    //first time opening this DB
-    //OR a new version was passed into open()
+function addMembers(memebers) {
+  if(getTeamMembers().length==0)
+    localStorage.setItem(MEMBERS, JSON.stringify(memebers));
+};
 
-    console.log('upgrade', db);
-    db = ev.target.result;
-    let oldVersion = ev.oldVersion;
-    let newVersion = ev.newVersion || db.version;
-    console.log('Database updated from version', oldVersion, 'to version', newVersion);
+var loggedInUser = (user) => {
+  localStorage.setItem("loggedInUser", JSON.stringify(user));
+}
 
-    if (db.objectStoreNames.contains('userStore')) {
-      db.deleteObjectStore('userStore');
-    }
 
-    //create the ObjectStore
-    objectStore = db.createObjectStore('userStore', {
-      keyPath: 'id',
-    });
 
-    //add the indexes
-    objectStore.createIndex('emailIDX', 'email', { unique: false });
-
-    if (db.objectStoreNames.contains('messageStore')) {
-      db.deleteObjectStore('messageStore');
-    }
-
-    //create the ObjectStore
-    objectStore = db.createObjectStore('messageStore', {
-      keyPath: 'mid',
-    });
-
-    if (db.objectStoreNames.contains('products')) {
-      db.deleteObjectStore('products');
-    }
-
-    //create the ObjectStore
-    objectStore = db.createObjectStore('products', {
-      keyPath: 'prodId',
-    });
-  });
-
-  function createTransaction(storeName, mode) {
-    let transaction = db.transaction(storeName, mode);
-    transaction.onerror = (err) => {
-      console.warn(err);
-    };
-    return transaction;
-  }
-
-  function insertProducts(){
-      if(!localStorage.getItem('allProducts')){
-        const listProducts = [
-          {
-            "name" :"Dress",
-            "price" : "$20.00",
-            "prodId": "001",
-            "description" : "Fabric cotton Rayon"
-          },
-          {
-            "name" :"Long Tunic Dress",
-            "price" : "$30.00",
-            "prodId": "002",
-            "description" : " Material: Cotton"
-          },
-          {
-            "name" :"Vintage Party Wear Mesh Skirt",
-            "price" : "$35.00",
-            "prodId": "003",
-            "description" : "Free size fits till 38'' bust"
-          },
-          {
-            "name" :"Imported top",
-            "price" : "$40.00",
-            "prodId": "004",
-            "description" : ""
-          }
-        ]
-        localStorage.setItem('listProducts',JSON.stringify(listProducts));
-      }
-  }
--- src/js/login_controller.js
+++ src/js/login_controller.js
@@ -1,14 +1,72 @@
-var IdsignInEmail = document.getElementById("signInEmail");
-var IdsignInPassword = document.getElementById("signInPassword");
-var managerEmail = "manager";
-var memberEmail = "member";
+
+
+var projectManagers =[]
+var members =[]
+
+var projectManager = {
+    name: "manager",
+    email: "manager@yopmail.com",
+    password: 12345,
+    role:"manager"
+   
+};
+projectManagers.push(projectManager)
+addManagers(projectManagers)
+
+var teamMemeber = {
+    name: "member",
+    email: "member@yopmail.com",
+    password: 12345,
+    role:"member"
+   
+};
+
+members.push(teamMemeber)
+addMembers(members)
+
+    
+
+
+// var managerEmail = "manager";
+// var memberEmail = "member";
 
 function performSignIn(){
+    var matchedUser =null
+    var signInEmail = document.getElementById("signInEmail").value;
+    var signInPassword = document.getElementById("signInPassword").value;
+    if(signInEmail=="")
+        alert("Enter Email First")
+    else if(signInPassword=="")
+        alert("Enter Password First")
+    else{
+        getManagers().forEach( manager => {
+            console.log(manager);
+            if(signInEmail == manager.email && signInPassword == manager.password) {
+                matchedUser = manager;
+                console.log(manager)
+            }
+        });
+        if(matchedUser==null){
+            getTeamMembers().forEach( member => {
+                console.log(member);
+                if(signInEmail == member.email && signInPassword == member.password) {
+                    matchedUser = member;
+                    console.log(member)
+                }
+            }); 
+        }
+        if(matchedUser==null){
+            alert("User not found")
+        }
 
-    if((IdsignInEmail.value).includes(managerEmail)){
-        window.location.href = "../../index.html";
-    } else if ((IdsignInEmail.value).includes(memberEmail)){
-        window.location.href = "../../member_dashboard.html";
+        if(matchedUser.role=="manager"){
+            window.location.href = "../../index.html";
+        }else{
+            window.location.href = "../../member_dashboard.html";
+        }
+
+
     }
+ 
 
 }
