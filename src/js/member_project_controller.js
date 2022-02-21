7f266db9495b1da62a3748674e70f5e4c8633d81
Sumit <sumitdesai1997@gmail.com>
2022-02-21 09:30:04 -0500

    on click of view task inside project module for member, showing only those task which are in current project for current user

--- src/js/member_project_controller.js
+++ src/js/member_project_controller.js
@@ -17,21 +17,16 @@ var userRelatedProjectList = projectList.filter(project => project.projectMember
 
 userRelatedProjectList.forEach(function callback(project, index) {
   console.log(project.projectName+"---"+project.projectDescription)
-  index++
 
-  let markup = "<tr><td>" +index + "</td><td><a>"+ project.projectName +"</a><br/><small>Created "+ project.startDate +"</small></td><td><ul class='list-inline'><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar2.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar3.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar4.png'></li></ul></td><td class='project_progress'><div class='progress progress-sm'><div class='progress-bar bg-green' role='progressbar' aria-valuenow='57' aria-valuemin='0' aria-valuemax='100' style='width: 57%'></div></div><small>57% Complete</small></td><td class='project-state'><span class='badge badge-success'>"+ project.projectStatus +"</span></td><td class='project-actions text-right'><a class='btn btn-primary btn-sm' href='./member_task.html'><i class='fas fa-folder'></i>View Task</a></td></tr>";
+  let markup = "<tr><td>" +index + "</td><td><a>"+ project.projectName +"</a><br/><small>Created "+ project.startDate +"</small></td><td><ul class='list-inline'><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar2.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar3.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar4.png'></li></ul></td><td class='project_progress'><div class='progress progress-sm'><div class='progress-bar bg-green' role='progressbar' aria-valuenow='57' aria-valuemin='0' aria-valuemax='100' style='width: 57%'></div></div><small>57% Complete</small></td><td class='project-state'><span class='badge badge-success'>"+ project.projectStatus +"</span></td><td class='project-actions text-right'><button class='btn btn-primary btn-sm'onclick='goToProjectRelatedTask("+index+")'><i class='fas fa-folder'></i>View Task</button></td></tr>";
   $("table tbody").append(markup);
   
+  index++
 });
 
-// var projectList = [
-//     { projectname : "Red chillies", date : "29/06/2022", completion :  "47% complete", status : "About to Success" },
-//     { projectname : "Green chillies", date : "22/06/2022", completion :  "57% complete", status : "Success" },
-//     { projectname : "Purple chillies", date : "19/06/2022", completion :  "80% complete", status : "Success" },
-//     { projectname : "Pink chillies", date : "11/06/2022", completion :  "95% complete", status : "Success" },
-// ]
+function goToProjectRelatedTask(index){
+  let currentProjectId = userRelatedProjectList[index].projectId;
 
-// for(let i = 0; i < projectList.length; i++){
-//     let markup = "<tr><td>" + i + "</td><td><a>"+ projectList[i].projectname +"</a><br/><small>Created "+ projectList[i].date +"</small></td><td><ul class='list-inline'><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar2.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar3.png'></li><li class='list-inline-item'><img alt='Avatar' class='table-avatar' src='../../dist/img/avatar4.png'></li></ul></td><td class='project_progress'><div class='progress progress-sm'><div class='progress-bar bg-green' role='progressbar' aria-valuenow='57' aria-valuemin='0' aria-valuemax='100' style='width: 57%'></div></div><small>57% Complete</small></td><td class='project-state'><span class='badge badge-success'>"+ projectList[i].status +"</span></td><td class='project-actions text-right'><a class='btn btn-primary btn-sm' href='./member_task.html'><i class='fas fa-folder'></i>View Task</a></td></tr>";
-//     $("table tbody").append(markup);
-// }
+  localStorage.setItem(CURRENT_PROJECT_ID, JSON.stringify(currentProjectId));
+  window.location.href = "./member_project_filtered_list.html";
+}
---
+++ src/js/member_project_filtered_list_controller.js
@@ -0,0 +1,23 @@
+var taskList = getTasks();
+var projectList = getProjects();
+
+var currentTaskId = JSON.parse(localStorage.getItem(CURRENT_TASK_ID));
+var currentProjectId =JSON.parse(localStorage.getItem(CURRENT_PROJECT_ID));
+
+var currentTask = taskList.filter(task => task.taskId == currentTaskId)[0];
+var currentProject = projectList.filter(project => project.projectId == currentProjectId)[0];
+
+var userRelatedCurrentProjectTaskList = currentProject.taskList.filter(task => task.taskMemberEmail == getCurrentUser().email);
+
+for(let i = 0; i < userRelatedCurrentProjectTaskList.length; i++){
+    let markup = "<tr><td> " + i + " </td><td><a>" + userRelatedCurrentProjectTaskList[i].taskName +"</a><br/><small>" + userRelatedCurrentProjectTaskList[i].startDate +"</small></td><td>" + userRelatedCurrentProjectTaskList[i].taskDescription + "</td><td class='project_progress'><div class='progress progress-sm'><div class='progress-bar bg-green' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width: 60%'></div></div><small>" + userRelatedCurrentProjectTaskList[i].left +"</small></td><td class='project-state'><span class='badge badge-success'>" + userRelatedCurrentProjectTaskList[i].taskStatus +"</span></td><td class='project-actions text-right'><button class='btn btn-info btn-sm' onclick='goToEditTask("+i+")'><i class='fas fa-pencil-alt'></i>Edit</a></td></tr>";
+    $("table tbody").append(markup);
+}
+
+function goToEditTask(index){
+    let currentTaskId = userRelatedCurrentProjectTaskList[index].taskId;
+    console.log("index: "+ index);
+
+    localStorage.setItem(CURRENT_TASK_ID, JSON.stringify(currentTaskId));
+    window.location.href = "./member_task_edit.html";
+}
